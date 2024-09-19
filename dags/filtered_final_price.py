f8c2bbb707938a4c8e88e82052e6290e9922d963
cma <linaauddk@naver.com>
2024-09-19 06:32:37 +0000

    중복제거

--- dags/filtered_final_price.py
+++ dags/filtered_final_price.py
@@ -41,29 +41,28 @@ def store_and_delete_data():
     conn = pymysql.connect(host=host, user=user, passwd=password, db=database)
     
     # Step 1: once_time 테이블에서 가장 최근 데이터를 가져옴
-    query_once_time = "SELECT * FROM `once_time` ORDER BY `date` DESC "
-    
+    query_once_time = "SELECT * FROM `once_time` ORDER BY `date` DESC"
     latest_data = pd.read_sql(query_once_time, conn)
     
     # Step 2: 가져온 데이터를 filtered_once_time 테이블에 저장
     latest_data = latest_data[['stock_code', 'name', 'closing_price', 'date']]
     latest_data.to_sql(name='filtered_once_time', con=engine, if_exists='append', index=False)
 
-    latest_data.to_sql(name='filtered_once_time', con=engine, if_exists='append', index=False)
     print(f"가장 최근 데이터를 filtered_once_time 테이블에 저장했습니다: {latest_data}")
     
-    # Step 3: filtered_once_time 테이블에서 가장 오래된 데이터 삭제
-    query_oldest = "SELECT `date` FROM `filtered_once_time` ORDER BY `date` ASC "
-    oldest_date = pd.read_sql(query_oldest, conn).iloc[0]['date']
-    
-    delete_query = f"DELETE FROM `filtered_once_time` WHERE `date` = '{oldest_date}'"
+    # Step 3: 가장 오래된 데이터를 찾아서 삭제
+    delete_query = """
+    DELETE FROM `filtered_once_time`
+    WHERE `date` = (SELECT `date` FROM `filtered_once_time` ORDER BY `date` ASC LIMIT 1)
+    """
     
     with conn.cursor() as cursor:
         cursor.execute(delete_query)
         conn.commit()
     
     conn.close()
-    print(f"오래된 데이터가 삭제되었습니다: {oldest_date}")
+    print(f"가장 오래된 데이터를 삭제했습니다.")
+
 
 # PythonOperator로 함수 실행 정의
 store_and_delete_data_task = PythonOperator(
